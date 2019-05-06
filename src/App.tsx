f0cdfa5a62bb614e5be7d131495c793ed4e2ffb7
dio <dhrubesh97@gmail.com>
2019-05-07 01:43:50 +0530

    added file-type check

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <FileUploader type="pdf" />
+        <FileUploader type="application/pdf" />
       </header>
     </div>
   );
--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -12,25 +12,30 @@ export class FileUploader extends React.Component<Props, {}> {
 
   handleChange(selectorFiles: any) {
     console.log(selectorFiles);
-    const fd = new FormData();
-    fd.append('file', selectorFiles[0], selectorFiles[0].name);
-    console.log('fd', fd);
-    axios
-      .post('http://localhost:5555/upload-file', fd, {
-        onUploadProgress: function(progressEvent) {
-          console.log('progressEvent', progressEvent);
-          let percentCompleted = Math.round(
-            (progressEvent.loaded * 100) / progressEvent.total
-          );
-          console.log('percentCompleted', percentCompleted);
-        },
-      })
-      .then(function(res) {
-        console.log('res', res);
-      })
-      .catch(function(err) {
-        console.error(err);
-      });
+    if (this.props.type === selectorFiles[0].type) {
+      const fd = new FormData();
+      console.log('this', this.props);
+      fd.append('file', selectorFiles[0], selectorFiles[0].name);
+      console.log('fd', fd);
+      axios
+        .post('http://localhost:5555/upload-file', fd, {
+          onUploadProgress: function(progressEvent) {
+            console.log('progressEvent', progressEvent);
+            let percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            console.log('percentCompleted', percentCompleted);
+          },
+        })
+        .then(function(res) {
+          console.log('res', res);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+    }
+    //Todo:
+    // handle else (throw error)
   }
 
   render() {
